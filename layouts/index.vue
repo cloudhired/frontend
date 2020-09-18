5ffde24c4be06ffdd1c706b352490fb5eab3c53f
gaomengen <gaomengen@gmail.com>
2020-09-17 20:39:43 -0400

    tweak signup and login

--- layouts/index.vue
+++ layouts/index.vue
@@ -15,27 +15,15 @@
 
         <div id="navbarBasicExample" class="navbar-menu">
             <div class="navbar-start">
-                <a class="navbar-item">Home</a>
-                <a class="navbar-item">Documentation</a>
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">More</a>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item">About</a>
-                        <a class="navbar-item">Jobs</a>
-                        <a class="navbar-item">Contact</a>
-                        <hr class="navbar-divider">
-                        <a class="navbar-item">Report an issue</a>
-                    </div>
-                </div>
+              <a class="navbar-item">Home</a>
+              <a class="navbar-item">Read</a>
             </div>
 
             <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button is-primary"><strong>Sign up</strong></a>
-                        <a href="https://cloudhired.com/login" class="button is-light">Log in</a>
-                    </div>
-                </div>
+              <div class="navbar-item">
+                  <a class="button is-white small-padding">Sign up</a>
+                  <a href="https://cloudhired.com/login" class="button is-white small-padding">Log in</a>
+              </div>
             </div>
         </div>
     </nav>
@@ -77,6 +65,15 @@ html {
 
 .navbar {
   width: 1200px;
+}
+
+.button.is-white {
+  font-size: smaller;
+}
+
+.button.small-padding {
+  padding-right: 0.4em;
+  padding-left: 0.4em;
 }
 
 .button--green {
