842c6750e081352dfb5affcf3c95b96366037ec2
gaomengen <gaomengen@gmail.com>
2020-09-29 09:05:01 -0400

    udpated table color and footer border

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-6">
-    <footer class="footer">
+    <footer class="footer main mt-6">
       <div class="content has-text-centered">
         <p>
           <strong>CloudHired</strong> by Morgan Gao. All rights reserved. 
@@ -9,3 +9,11 @@
     </footer>
   </div>
 </template>
+
+<style lang="scss">
+
+.footer.main {
+  border-top: solid 1px rgba($color: #cdcfd2, $alpha: .7);
+}
+
+</style>
