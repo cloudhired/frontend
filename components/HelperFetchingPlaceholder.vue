505e1353864911ad9050dcc4d6b6587f22fb8a6c
gaomengen <gaomengen@gmail.com>
2020-10-15 23:12:43 -0400

    added loading placeholder

---
+++ components/HelperFetchingPlaceholder.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section profile" style="height: 100vh;">
+    <div class="fetching-holder">
+      {{ msg }}
+    </div>
+  </section>
+</template>
+
+
+<script>
+export default {
+  props: ['msg']
+}
+</script>
+
+<style lang="scss">
+
+.fetching-holder {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -50px;
+    margin-left: -50px;
+}
+
+</style>
--- pages/ch/_id.vue
+++ pages/ch/_id.vue
@@ -1,5 +1,6 @@
 <template>
-  <section v-if="$fetchState.pending" class="section profile" style="height: 100vh;">Fetching information...</section>
+  <!-- <section v-if="$fetchState.pending" class="section profile" style="height: 100vh;"><div style="margin-top:50%">Fetching information...</div></section> -->
+  <HelperFetchingPlaceholder v-if="$fetchState.pending" msg="Fetching information..."/>
   <section v-else class="section profile">
     <div class="columns profile">
       <div class="column is-two-thirds pl-0">
