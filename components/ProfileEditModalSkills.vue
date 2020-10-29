819169210763a6b8a639b6d4f9925cca2cabfe0c
gaomengen <gaomengen@gmail.com>
2020-10-28 23:04:15 -0400

    almost done with skills editing

--- components/ProfileEditModalSkills.vue
+++ components/ProfileEditModalSkills.vue
@@ -7,17 +7,21 @@
         <button v-on:click="toogleEditModal(editModalId)" class="delete" aria-label="close"></button>
       </header>
       <section class="modal-card-body">
+        <div class="field is-grouped mx-4 mb-4">
+          <input v-model="skillToAdd" class="input is-small mr-2" type="text" placeholder="Type a skill you have here">
+          <button v-on:click="addSkill()" class="button is-small">Add Skill</button>
+        </div> 
         <div class="field is-grouped is-grouped-multiline mx-4 my-4">
-          <div class="control" v-for="skill in userInfo.skills" v-bind:key="skill">
+          <div class="control" v-for="skill in skills" v-bind:key="skill">
               <div class="tags has-addons">
               <a class="tag is-link"> {{ skill }} </a>
-              <a class="tag is-delete"></a>
+              <a v-on:click="delSkill(skill)" class="tag is-delete"></a>
             </div>
           </div>
         </div>
       </section>
       <footer class="modal-card-foot">
-        <button class="button is-success">Save changes</button>
+        <button v-on:click="sbtskillss()" class="button is-primary">Save changes</button>
         <button v-on:click="toogleEditModal(editModalId)" class="button">Cancel</button>
       </footer>
     </div>
@@ -31,28 +35,45 @@ export default {
 
   data() {
     return {
-      intro: this.userInfo.intro, 
-      introInfo: {}
+      skillToAdd: '', 
+      skills: this.userInfo.skills, 
+      skillsInfo: {}
     }
   },
 
   methods: {
-    sbtIntro: async function () {
-      //TODO: validate each blank
+    delSkill: function (skill) {
+      const index = this.skills.indexOf(skill);
+        if (index > -1) {
+          this.skills.splice(index, 1);
+        }
+    },
+    addSkill: function () {
+      const index = this.skills.indexOf(this.skillToAdd);
+        if (index == -1) {
+          this.skills.push(this.skillToAdd);
+        }
+      console.log(this.skills)
+    },
+    sbtSkillss: function () {
+      console.log("it is a function")
+    },
+    sbtSkills: async function () {
       if (this.isPageOwner()) {
-        this.intro ? this.introInfo.intro = this.intro : null;
-        // submit change if basic info is not null
-        if (Object.keys(this.introInfo).length != 0) {
+        // TODO: since var like `skills` has initial value (not null ever), need to change the logic to check if changed. 
+        this.skills ? this.skillsInfo.skills = this.skills : null;
+        // submit change if info is changed
+        if (Object.keys(this.skillsInfo).length != 0) {
           let payload = {}
           payload.email = this.userInfo.email 
-          payload.setInfo = this.introInfo
+          payload.setInfo = this.skillsInfo
           console.log(payload)
           await this.$http.$post(`/api/user/${this.$route.params.id}`, payload)
           .then((res) => {
             console.log("Finished", res)
             if (res.error == null ) {
               this.toogleEditModal(this.editModalId)
-              this.updateUserInfo(this.introInfo)
+              this.updateUserInfo(this.skillsInfo)
             } else {
               // TODO: remind user of error when updating
               alert("not successful")
