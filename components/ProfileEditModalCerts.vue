b914d675f55dac17b0da32a94d6ee4db04b31c40
Mengen Gao <gaomengen@gmail.com>
2021-01-30 23:27:42 -0500

    updated certification adding page

--- components/ProfileEditModalCerts.vue
+++ components/ProfileEditModalCerts.vue
@@ -7,11 +7,14 @@
         <button v-on:click="toogleEditModal(editModalId)" class="delete" aria-label="close"></button>
       </header>
       <section class="modal-card-body">
-        <div class="select is-primary">
-          <select>
-            <option>Select dropdown</option>
+        <div class="select is-primary" style="width:100%">
+          <select style="width:100%">
+            <option>Select a certificate to add from dropdown</option>
             <option>With options</option>
           </select>
+        </div>
+        <div class="mt-1" style="width:100%">
+          <input class="input is-primary" type="text" placeholder="https://">
         </div>
       </section>
       <footer class="modal-card-foot">
@@ -30,8 +33,12 @@ export default {
   data() {
     return {
       isInEdit: false, 
+      certs: [],
     }
   },
+  async fetch() {
+    this.certs = await this.$http.$get('https://api.nuxtjs.dev/posts')
+  }
 }
 </script>
 
